167f3faccbb1458ab631b322a0a4714d43add948
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-04-10 13:54:29 +0900

    Create libft.h for libft-01

--- libft.h
+++ libft.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/06 19:25:52 by kfujita           #+#    #+#             */
-/*   Updated: 2022/04/06 20:03:41 by kfujita          ###   ########.fr       */
+/*   Created: 2022/04/10 13:37:46 by tetsu             #+#    #+#             */
+/*   Updated: 2022/04/10 13:44:08 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 // Dependency:
 // - size_t
-# include <string.h>
+# include <stddef.h>
 
 // Libft-00
 int		ft_isalpha(int c);
@@ -33,5 +33,21 @@ void	*ft_memmove(void *dst, const void *src, size_t n);
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+
+// Libft-01
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+
+int		ft_strncmp(const char *s1, const char *s2);
+
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memcmp(const void *s1, const void *s2, size_t n);;
+
+char	*ft_strstr(const char *haystack, const char *needle);
+
+int		ft_atoi(const char *str);
 
 #endif
