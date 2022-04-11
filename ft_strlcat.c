3b1f38586ecf9c47eae2df9d710989247e4b69b6
kfujita <kfujita@student.42tokyo.jp>
2022-04-12 00:42:09 +0900

    dstの長さがmax_lenよりも長い場合に、max_lenよりも先までアクセスしてしまうバグを修正

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,18 +6,19 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 20:11:16 by kfujita           #+#    #+#             */
-/*   Updated: 2022/04/06 23:29:53 by kfujita          ###   ########.fr       */
+/*   Updated: 2022/04/11 20:48:58 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include "libft.h"
 
-static size_t	ft_strlen(const char *str)
+static size_t	ft_strnlen(const char *str, size_t max_len)
 {
 	size_t	len;
 
 	len = 0;
-	while (*str != '\0')
+	while (len < max_len && *str != '\0')
 	{
 		str++;
 		len++;
@@ -30,10 +31,7 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	size_t	src_len;
 	size_t	dest_len;
 
-	if (size != 0)
-		dest_len = ft_strlen(dest);
-	else
-		dest_len = 0;
+	dest_len = ft_strnlen(dest, size);
 	src_len = ft_strlen(src);
 	if (dest_len >= size)
 		return (size + src_len);
