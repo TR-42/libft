edc4db821aaa307d7049c36e4115bad7ac45cd15
kfujita <kfujita@student.42tokyo.jp>
2022-04-19 08:10:50 +0900

    Complete libft-03

---
+++ ft_putstr_fd.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/19 07:57:56 by kfujita           #+#    #+#             */
+/*   Updated: 2022/04/19 07:59:12 by kfujita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	length;
+
+	if (s == NULL)
+		return ;
+	length = ft_strlen(s);
+	write(fd, s, length);
+}
---
+++ ft_strsteri.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/19 07:54:31 by kfujita           #+#    #+#             */
+/*   Updated: 2022/04/19 07:56:52 by kfujita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	size_t	index;
+
+	if (s == NULL || f == NULL)
+		return ;
+	index = 0;
+	while (*s != '\0')
+		f(index++, s++);
+}
